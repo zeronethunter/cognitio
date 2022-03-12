6f8f85a556c6dd0a5ebb43944ba88d3f651f42ab
Nikita Zheleztsov <nikita.zheleztsov@mail.ru>
2022-03-12 14:37:10 +0300

    Add git submodule

---
+++ cmake/GitSubmodule.cmake
@@ -0,0 +1,15 @@
+if(GIT_SUBMODULE)
+  find_package(Git QUIET)
+  if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
+    message(STATUS "Submodule update")
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      RESULT_VARIABLE GIT_SUBMOD_RESULT)
+
+    if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+      message(FATAL_ERROR "git submodule update --init --recursive failed with ${GIT_SUBMOD_RESULT}")
+    endif()
+  else()
+    message(WARNING "Not a git repository or git wasn't found")
+  endif()
+endif()
